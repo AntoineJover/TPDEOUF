6239861f347a00f8767379279c55407c243c1337
AntoineJover <antoine.jover@hotmail.fr>
2014-04-13 17:07:07 +0200

    ajout

---
+++ TP2/src/Paul/Labyrinthe.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Paul;
+
+import java.util.*;
+import java.io.*;
+        
+/**
+ *
+ * @author Kreax
+ */
+public class Labyrinthe {
+    
+   public int tailleX, tailleY ;
+public int departX, departY ;
+public int arriveeX, arriveeY; 
+private int posX, posY ;
+
+    
+    
+    
+    public void InitFromFile(File lab) throws FileFormatException
+{
+Scanner laby = new Scanner(lab);
+String parametre = new String();
+parametre = laby.nextLine();
+
+// test
+
+
+}
+    
+    
+}
